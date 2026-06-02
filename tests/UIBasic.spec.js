e5996a8da84e677cb852d7079cf04170be8bfa01
Nikam, Utkarsha <utkarsha.nikam+ralliant@ralitservices.com>
2026-06-02 12:02:12 +0530

    Update Browser Context Test with login functionality and adjust test settings

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -1,14 +1,19 @@
 const {test,expect}= require('@playwright/test');
 
-test('Browser Context Test',async ({browser})=>{
+test.only('Browser Context Test',async ({browser})=>{
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
     console.log(await page.title());
+    await page.locator('#username').fill('dsad');
+    await page.locator("[type='password']").fill('sadsd');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await page.locator("[style*='block']").toContainText("ddd");
 
 });
 
-test.only('Page Test',async ({page})=>{
+test('Page Test',async ({page})=>{
     await page.goto('https://google.com');
     console.log(await page.title());
     await expect(page).toHaveTitle('Google');
