979d8d3d04b2d7a3ff55c35128b7487734062418
Nikam, Utkarsha <utkarsha.nikam+ralliant@ralitservices.com>
2026-06-02 16:17:42 +0530

    Enhance Assignment 1 Test to wait for network idle state before logging output

--- tests/assignem1.spec.js
+++ tests/assignem1.spec.js
@@ -17,5 +17,8 @@ test.only('Assignment 1 Test',async ({page})=>{
     await page.locator('#userEmail').fill('utkarshanikam9@gmail.com');
     await page.locator('#userPassword').fill('Utkarsha@123');
     await page.locator('#login').click();
-    console.log(await page.locator('.card-body b').nth(0).textContent());
+
+    await page.waitForLoadState('networkidle');
+
+    console.log(await page.locator('.card-body b').nth(0).allTextContents());
 });
